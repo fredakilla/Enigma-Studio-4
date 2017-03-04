48357dd9de1f806591e805e8ec8b445beb613ec6
fredakilla <fredakilla@gmail.com>
2017-03-04 11:08:23 +0100

    shaderc test compile

--- binary/shaders/BuildShaders.bat
+++ binary/shaders/BuildShaders.bat
@@ -2,23 +2,23 @@
 
 set OUTPWD=.
 set PLATFORM=windows
-set SHADERC="C:\github\bgfx\.build\win32_vs2015\bin\shadercRelease.exe"
+set SHADERC="shadercRelease.exe"
 
 IF NOT EXIST %OUTPWD% mkdir %OUTPWD%
 
-set DIRSRC=Shaders\OpsWz4Shaders\TextureOperator
+set DIRSRC=bgfx
 
 echo compiling TextureOperator
-%SHADERC% -f  %DIRSRC%\OpsWz4_VS_RenderTexture.sc -o  %OUTPWD%\OpsWz4_VS_RenderTexture.bin --varyingdef %DIRSRC%\varying.def.sc --type v --platform %PLATFORM%
-%SHADERC% -f  %DIRSRC%\OpsWz4_FS_RenderTexture.sc -o  %OUTPWD%\OpsWz4_FS_RenderTexture.bin --varyingdef %DIRSRC%\varying.def.sc --type f --platform %PLATFORM%
+%SHADERC% -f  %DIRSRC%\vs_cubes.sc -o  %OUTPWD%\vs_cubes.bin --varyingdef %DIRSRC%\varying.def.sc --type v --platform %PLATFORM%
+%SHADERC% -f  %DIRSRC%\fs_cubes.sc -o  %OUTPWD%\fs_cubes.bin --varyingdef %DIRSRC%\varying.def.sc --type f --platform %PLATFORM%
 
 
-set DIRSRC=Shaders\OpsWz4Shaders\MeshOperator
+REM set DIRSRC=Shaders\OpsWz4Shaders\MeshOperator
 
-echo compiling MeshOperator 1
-%SHADERC% -f  %DIRSRC%\OpsWz4_VS_OperatorMesh.sc -o  %OUTPWD%\OpsWz4_VS_OperatorMesh.bin --varyingdef %DIRSRC%\varying.def.sc --type v --platform %PLATFORM%
-echo compiling MeshOperator 2
-%SHADERC% -f  %DIRSRC%\OpsWz4_FS_OperatorMesh.sc -o  %OUTPWD%\OpsWz4_FS_OperatorMesh.bin --varyingdef %DIRSRC%\varying.def.sc --type f --platform %PLATFORM%
+REM  echo compiling MeshOperator 1
+REM %SHADERC% -f  %DIRSRC%\OpsWz4_VS_OperatorMesh.sc -o  %OUTPWD%\OpsWz4_VS_OperatorMesh.bin --varyingdef %DIRSRC%\varying.def.sc --type v --platform %PLATFORM%
+REM echo compiling MeshOperator 2
+REM  %SHADERC% -f  %DIRSRC%\OpsWz4_FS_OperatorMesh.sc -o  %OUTPWD%\OpsWz4_FS_OperatorMesh.bin --varyingdef %DIRSRC%\varying.def.sc --type f --platform %PLATFORM%
 
 pause
 
